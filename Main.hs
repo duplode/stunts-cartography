eec3b512b9cc49112468cc9696916ca672ded1c5
Daniel Mlot <scrstunts@yahoo.com.br>
2013-02-02 00:23:24 -0200

    Cleanup of grid line rendering, placing global origin at BL.

--- Main.hs
+++ Main.hs
@@ -11,12 +11,11 @@ import Pics
 import Utils
 
 main = do
-    trkBS <- LB.readFile "ZCT106.TRK"
+    trkBS <- LB.readFile "CYDONIA.TRK"
     let rawTrk = veryRawReadTrack trkBS
         tilArr = rawTrackToTileArray rawTrk
     --putStrLn . show $ tilArr ! (4, 7) -- Why are the indices swapped?
     let rows = map snd <$> splitAtEvery30th (assocs tilArr)
-        -- TODO: Do this in a single pass (can arrows help?)
         terrRows = cat unitX <$> (map getTerrainPic <$> rows)
         terrFull = cat unitY terrRows
         elmsRows = cat' unitX with { sep = 1, catMethod = Distrib }
@@ -24,18 +23,9 @@ main = do
         elmsFull = cat' unitY with { sep = 1, catMethod = Distrib } elmsRows
     defaultMain $
         (elmsFull <> terrFull)
-        # centerXY
-        # atop (vcat' with { sep = 1 } (replicate 29 $ hrule 30) # centerY)
-        # atop (hcat' with { sep = 1 } (replicate 29 $ vrule 30) # centerX)
+        # alignBL
+        # atop gridLines
 
-
--- Hand-rolled:
-{-
-group30TileRows :: [((Int, Int), Tile)] -> [] [((Int, Int), Tile)]
-group30TileRows arrAscs =
-    map fst . takeWhile (not . null . fst) . drop 1
-    . iterate (splitAt 30 . snd) $ ([], arrAscs)
-
-obviousGroupRows :: [((Int, Int), Tile)] -> [] [((Int, Int), Tile)]
-obviousGroupRows = groupBy ((==) `on` (fst . fst))
--}
+gridLines =
+    vcat' with { sep = 1 } (replicate 31 $ hrule 30) # alignBL
+    <> hcat' with { sep = 1 } (replicate 31 $ vrule 30) # alignBL
