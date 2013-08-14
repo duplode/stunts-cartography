f12e3efb5a449bc8d5e6e192611d83bac0544fe6
Daniel Mlot <duplode_1@yahoo.com.br>
2013-08-14 13:36:05 -0300

    A split rendering helper constructor.

--- Annotate.hs
+++ Annotate.hs
@@ -24,7 +24,7 @@ renderAnnotation ann = case ann of
         ->
         acura' colour 1
         # rotate (Deg angle)
-        # (flip $ beside (captAlignToDirection captAlign))
+        # (flip $ beside (captAlignToR2 captAlign))
             (renderCaption colour captAlign captAngle captRelSize caption)
         # scale size
         # translate (r2 pos)
@@ -35,20 +35,31 @@ renderAnnotation ann = case ann of
         fromSegments [ straight (r2 (len, 0) # rotate (Deg angle)) ]
         # stroke
         # lw 0.25 # lc colour
-        # (flip $ beside (captAlignToDirection captAlign))
+        # (flip $ beside (captAlignToR2 captAlign))
             (renderCaption colour captAlign captAngle captSize caption)
         # translate (r2 pos)
+
+    Split colour ix (tileLX, tileBY) splitDir len captAlign
+        -> renderAnnotation $
+        Seg colour (fromIntegral tileLX, fromIntegral tileBY)
+            (splitDirectionToAngle splitDir) (fromIntegral len)
+            (show ix) captAlign 0 0.5
 
 renderCaption colour captAlign captAngle captSize caption =
     text caption # scale captSize # rotate (Deg captAngle)
     # fc colour
     -- TODO: Changing the rectangle size doesn't seem to change padding.
     <> rect captSize captSize # lw 0
-    # align (captAlignToDirection captAlign)
+    # align (captAlignToR2 captAlign)
 
-captAlignToDirection :: CaptionAlignment -> R2
-captAlignToDirection x = case x of
+captAlignToR2 :: CaptionAlignment -> R2
+captAlignToR2 x = case x of
     E -> unitX
     N -> unitY
     W -> unit_X
     S -> unit_Y
+
+splitDirectionToAngle :: SplitDirection -> Double
+splitDirectionToAngle x = case x of
+    H -> 0
+    V -> 90
--- AnnotationTypes.hs
+++ AnnotationTypes.hs
@@ -10,10 +10,16 @@ data CaptionAlignment = E
                       | S
                       deriving (Read, Show, Eq, Ord)
 
+data SplitDirection = H
+                    | V
+                    deriving (Read, Show, Eq, Ord)
+
 -- TODO: Do something to have better constructors.
 -- Maybe add a wrapper typeclass, or play with existentials to hide the fields.
 data Annotation = Car (Colour Double) (Double, Double) Double Double
                       String CaptionAlignment Double Double
                 | Seg (Colour Double) (Double, Double) Double Double
                       String CaptionAlignment Double Double
+                | Split (Colour Double) Int (Int, Int) SplitDirection Int
+                        CaptionAlignment
                 deriving (Read, Show)
