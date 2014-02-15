4df4d458fa0555db17dd1c17b87d450fb4d2ca61
Daniel Mlot <duplode_1@yahoo.com.br>
2014-02-15 03:27:44 -0200

    Stripping the directory from the zip path entries.
    This workaround mostly solves the issue pointed out in
    e99878672a733ed8f6fe0bdbade5619112e41b6c . Leaving one directory level within
    the zip might be polite. A patch to zip-conduit with a proper implementation
    might be a worthy endeavour.

--- src/Util/ZipConduit.hs
+++ src/Util/ZipConduit.hs
@@ -4,14 +4,25 @@ module Util.ZipConduit
     ) where
 
 import Control.Monad (filterM)
+import Control.Monad.Trans (liftIO)
 import Control.Applicative ((<$>))
 import System.Directory (doesFileExist, getDirectoryContents)
 import System.FilePath ((</>), takeFileName)
 import "zip-conduit" Codec.Archive.Zip
+import Data.Conduit
+import qualified Data.Conduit.List as CL
+import qualified Data.Conduit.Binary as CB
+import qualified Data.ByteString.Lazy as BL
 
 writeDirContentsZip :: FilePath -> FilePath -> IO ()
 writeDirContentsZip dir dest = do
     -- TODO: No existence check for dir.
     paths <- filterM doesFileExist
         =<< (map (dir </>)) <$> getDirectoryContents dir
-    withArchive dest $ addFiles paths
+    withArchive dest $
+        CL.sourceList paths $$ sinkFileWithChangedPath takeFileName
+
+sinkFileWithChangedPath :: (FilePath -> FilePath) -> Sink FilePath Archive ()
+sinkFileWithChangedPath fPath =
+    CL.mapM (\p -> liftIO (BL.readFile p) >>= \bs -> return (fPath p, bs))
+        =$ CL.mapM_ (\(p, bs) -> sinkEntry p $ CB.sourceLbs bs)
