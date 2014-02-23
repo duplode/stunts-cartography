7c9c828a7160dea0b5fbe39d93a062f5a50db4dd
Daniel Mlot <duplode_1@yahoo.com.br>
2014-02-22 23:17:48 -0300

    The repldump batch script should overwrite temporary and destination files.

--- repldump/repldump.bat
+++ repldump/repldump.bat
@@ -1,7 +1,7 @@
 @echo off
 if not -%1-==-- (
-  copy %1 STUNTS\tmp-dump.rpl
+  copy /y %1 STUNTS\tmp-dump.rpl
   "C:\Program Files\DOSBox-0.74\DOSBox.exe" -conf repldump.conf
-  move STUNTS\tmp-dump.bin .
-  for %%f in (%1) do rename tmp-dump.bin %%~nf.bin
+  move /y STUNTS\tmp-dump.bin .
+  for %%f in (%1) do move /y tmp-dump.bin %%~nf.bin
 )
