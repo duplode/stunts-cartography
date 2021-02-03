e436dac1eae56fd1718ec453f855af62925375f2
Daniel Mlot <duplode_1@yahoo.com.br>
2021-02-03 14:55:51 -0300

    Using th-lift-instances instead of file-embed for ByteString TH-lifting.

--- src/Util/SVGFonts.hs
+++ src/Util/SVGFonts.hs
@@ -3,16 +3,16 @@ module Util.SVGFonts where
 
 import qualified Graphics.SVGFonts.Fonts as SVGFonts (bit)
 import Graphics.SVGFonts.ReadFont (PreparedFont)
-import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
+import Instances.TH.Lift
 import Data.Serialize
-import Data.FileEmbed (bsToExp)
 import Data.Either (fromRight)
 
--- An alternative to unsafePerformIO SVGFonts.bit
+-- A compile-time alternative to unsafePerformIO SVGFonts.bit
 -- Note that restricting this to Double might limit the range of
--- compatible diagrams backends.
+-- compatible diagrams backends. We might eventually need variants for
+-- different choices of n.
 bit :: PreparedFont Double
 bit = fromRight
     (error "Util.SVGFonts.Bit: deserialization failure")
-    (decode $(runIO SVGFonts.bit >>= bsToExp . encode @(PreparedFont Double)))
+    (decode $(runIO SVGFonts.bit >>= lift . encode @(PreparedFont Double)))
