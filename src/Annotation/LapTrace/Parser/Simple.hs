834b027aa9fbb648c59cd28f89a2a16be8d80a93
Daniel Mlot <duplode_1@yahoo.com.br>
2021-11-02 22:30:11 -0300

    Revert "Mention trackdata2carto in the trace parser notes"
    This reverts commit ec56e242194bf39126e4a1dc50d58f17593aa23d.
    
    The comment added there was plain wrong.

--- src/Annotation/LapTrace/Parser/Simple.hs
+++ src/Annotation/LapTrace/Parser/Simple.hs
@@ -25,8 +25,7 @@ import Annotation.LapTrace.Vec
 --     * The optional integers are the player speed and gear.
 --
 -- The speed and gear fields are optional so that the parser accepts
--- repldump2carto output from version 0.4, as well as trackdata2carto
--- output.
+-- repldump2carto output from version 0.4.
 --
 -- All values are assumed to be in the appopriate internal game units.
 
